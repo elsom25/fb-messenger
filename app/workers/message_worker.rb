946978909dbf95a77360e6a33aa8a24c228e5876
Jesse McGinnis <jessieelsom@hotmail.com>
2014-01-10 00:58:39 -0500

    Cleaned up worker class to work with sidekiq.

--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -1,34 +1,32 @@
 class MessageWorker
   include Sidekiq::Worker
 
-  def initialize(sender_uid, sender_token, receiver_uid, message_body, message_subject=nil)
-    @sender_uid = sender_uid
-    @sender_token = sender_token
-    @receiver_uid = receiver_uid
-    @message_body = message_body
-    @message_subject = message_subject if message_subject
-
+  def perform(sender_uid, sender_token, receiver_uid, message_body, message_subject=nil)
     flush 'Initialized MessageWorker instance'
-  end
 
-  def send
-    message = create_message
+    message = create_message(receiver_uid, message_body, message_subject)
 
-    client = create_client
+    client = create_client(sender_uid, sender_token)
     client.send message
     client.close
 
     flush "Message sent"
   end
 
-  def perform(*args)
-    (new *args).send
-  end
-
 protected
 
-  def create_client
-    sender_chat_id = "-#{@sender_uid}@chat.facebook.com"
+  def create_message(receiver_uid, message_body, message_subject)
+    receiver_chat_id = "-#{receiver_uid}@chat.facebook.com"
+
+    message = Jabber::Message.new receiver_chat_id, message_body
+    message.subject = message_subject if message_subject
+
+    flush "Message created: #{message_body}"
+    message
+  end
+
+  def create_client(sender_uid, sender_token)
+    sender_chat_id = "-#{sender_uid}@chat.facebook.com"
     client = Jabber::Client.new(Jabber::JID.new(sender_chat_id))
 
     client.connect
@@ -36,23 +34,13 @@ protected
       Jabber::SASL::XFacebookPlatform.new(
         client,
         CONFIG['fb_app_id'],
-        @sender_token,
+        sender_token,
         CONFIG['fb_secret_key']
       ),
       nil
     )
 
     client
-  end
-
-  def create_message
-    receiver_chat_id = "-#{@receiver_uid}@chat.facebook.com"
-
-    message = Jabber::Message.new receiver_chat_id, @body
-    message.subject = @subject if @subject
-
-    flush "Message created: #{@body}"
-    message
   end
 
   def flush(str)
